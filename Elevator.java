0c17c42373b5ad64aa11d1c92a0d0abaa13ae1e2
Evgeni Vasilev <evgeni.vasilev0912@gmail.com>
2020-12-04 21:56:23 +0200

    Complete code

--- Elevator.java
+++ Elevator.java
@@ -1,6 +1,10 @@
 /**
  * @author Evgeni Vasilev
- *
+ * Run the program from cmd and follow the instructions
+ * written in the console
+ * If use IDE - NetBeans
+ * press F6 key and follow the instructions
+ * written in the console
  *
  */
 package elevator;
@@ -26,8 +30,8 @@ public class Elevator {
      */
     private static void procesMoveUpMoveDownEventsRegular(String pickElevator)
             throws InterruptedException {
-        if (String.valueOf(pickElevator).equals("A")
-                || String.valueOf(pickElevator).equals("B")) {
+        if (pickElevator.toUpperCase().equals("A")
+                || pickElevator.toUpperCase().equals("B")) {
             System.out.println("Please select a floor: ");
             Scanner sc = new Scanner(System.in);
             int choosenFloor = sc.nextInt();
@@ -35,8 +39,8 @@ public class Elevator {
                 Timer t = new Timer();
                 Instant start = Instant.now();
                 setTimer(choosenFloor);
-                System.out.println(pickElevator + " moves to: " + choosenFloor);
-                System.out.println(pickElevator + " doors open.");
+                System.out.println(pickElevator.toUpperCase() + " moves to: " + choosenFloor);
+                System.out.println(pickElevator.toUpperCase() + " doors open.");
                 if (seconds == 1) {
                     System.out.println("-");
                 } else if (seconds != 1) {
@@ -47,9 +51,9 @@ public class Elevator {
                 t.cancel();
                 Instant end = Instant.now();
                 Duration interval = Duration.between(start, end);
-                System.out.println(pickElevator + " arives on "
+                System.out.println(pickElevator.toUpperCase() + " arives on "
                         + choosenFloor);
-                System.out.println(pickElevator + " close doors.");
+                System.out.println(pickElevator.toUpperCase() + " close doors.");
                 System.out.println("Seconds spent in elevator: "
                         + interval.getSeconds());
             } else {
@@ -118,6 +122,35 @@ public class Elevator {
     }
 
     /**
+     * Emulate and process rescue mode
+     *
+     * @param pickElevator
+     */
+    private static void ProcessRescueMode(String pickElevator) {
+        // A elevator
+        if (floor >= (-1) && floor <= 10) {
+            System.out.println("Emergency button pushed in elevator " + pickElevator.toUpperCase());
+            if (floor == (-1)) {
+                resqueFloor = floor + 1;
+                System.out.println(pickElevator.toUpperCase() + " moves to: " + resqueFloor);
+            } else {
+                resqueFloor = floor - 1;
+                System.out.println(pickElevator.toUpperCase() + " moves to: " + resqueFloor);
+            }
+        }
+        // B elevator
+        if (floor >= 0 && floor <= 9) {
+            System.out.println("Emergency button pushed in elevator " + pickElevator.toUpperCase());
+            if (floor == 0) {
+                resqueFloor = floor + 1;
+                System.out.println(pickElevator.toUpperCase() + " moves to: " + resqueFloor);
+            } else {
+                resqueFloor = floor - 1;
+            }
+        }
+    }
+
+    /**
      * @param args the command line arguments
      * @throws java.lang.InterruptedException
      */
@@ -129,11 +162,11 @@ public class Elevator {
                 + " 'E' for emergency mode:");
         Scanner chooseModeScanner = new Scanner(System.in);
         String choosenMode = chooseModeScanner.next();
-        switch (setMode(choosenMode)) {
+        switch (setMode(choosenMode.toUpperCase())) {
             case "Regular": {
-                // REGULAR MODE PROCESSINF
+                // REGULAR MODE PROCESSING
                 System.out.println("Please pick an elevator. Elevator \"A\" "
-                        + "goes from 0 to 9th floor. Elevator \"B\" "
+                        + "goes from 0 to 9th floor and -1 level. Elevator \"B\" "
                         + "goes from 0 to 10th floor (elevator is on "
                         + floor + " floor)");
                 Scanner pickElevatorScanner = new Scanner(System.in);
@@ -142,54 +175,56 @@ public class Elevator {
                 break;
             }
             case "Emergency": {
+                // EMERGENCY MODE PROCESSING
                 System.out.println("Please pick an elevator. Elevator \"A\" "
-                        + "goes from 0 to 9th floor. Elevator \"B\" "
+                        + "goes from 0 to 9th floor and -1 level. Elevator \"B\" "
                         + "goes from 0 to 10th floor (elevator is on "
                         + floor + " floor)");
                 Scanner pickElevatorScanner = new Scanner(System.in);
                 String pickElevator = pickElevatorScanner.nextLine();
+                // Type ER to emulate Reset button pushed
                 System.out.println("Emulate push \"Emergeny Button\" by typing ER");
 
                 Scanner pushEmrBtnScanner = new Scanner(System.in);
                 String pushErmBtn = pushEmrBtnScanner.nextLine();
 
-                if (String.valueOf(pushErmBtn).equals("ER")) {
+                if (pushErmBtn.toUpperCase().equals("ER")) {
                     Instant start = Instant.now();
-                    // A elevator
-                    if (floor > (-1) && floor <= 9) {
-                        System.out.println("Passenger is in elevator \"A\"");
-                        if (floor == (-1)) {
-                            resqueFloor = floor + 1;
-                            System.out.println(pickElevator + " moves to: " + resqueFloor);
-                        } else {
-                            resqueFloor = floor - 1;
-                            System.out.println(pickElevator + " moves to: " + resqueFloor);
-                        }
-                    }
-                    System.out.println(pickElevator + " doors open.");
+                    ProcessRescueMode(pickElevator.toUpperCase());
+                    System.out.println(pickElevator.toUpperCase() + " doors open.");
                     System.out.println("-");
                     TimeUnit.SECONDS.sleep(1);
 
                     Instant end = Instant.now();
                     Duration interval = Duration.between(start, end);
-                    System.out.println(pickElevator + " arives on "
+                    System.out.println(pickElevator.toUpperCase() + " arives on "
                             + resqueFloor);
-                    System.out.println(pickElevator + " doors open.");
+                    System.out.println(pickElevator.toUpperCase() + " doors open.");
                     System.out.println("Seconds spent in elevator: "
                             + interval.getSeconds());
+                    System.out.println("Type \"RST\" to close the door, please!");
+                    // Type RST to emulate Reset button pushed
                     Scanner pushResetBtnScanner = new Scanner(System.in);
                     String pushResetBtn = pushResetBtnScanner.nextLine();
-
+                    ProcessReset(pushResetBtn, pickElevator);
                 }
                 break;
             }
             case "Wrong":
+                // IN CASE WRONG INPUT WAS GIVEN
                 System.out.println("No such an elevator!");
                 break;
             default:
-                System.out.println("Opps!");
+                System.out.println("Oops!");
                 break;
         }
+    }
 
+    private static void ProcessReset(String pushResetBtn, String pickElevator) {
+        if (pushResetBtn.toUpperCase().equals("RST")) {
+            System.out.println(pickElevator.toUpperCase() + " dooor close.");
+        } else {
+            System.out.println(pickElevator.toUpperCase() + " is still opened.");
+        }
     }
 }
